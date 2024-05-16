79704ef6780a322bc592159cb29918cc5c29e291
rvanesa <palominovanesa82@gmail.com>
2024-05-16 09:37:27 -0500

    asda

--- nosotros.php
+++ nosotros.php
@@ -20,33 +20,85 @@
 
             <body>
             <?php include('include/menu.php'); ?>
+            
                 <br><br><br>
-                <div class="container my-5">
-                    <div class="row">
-                        <div class="col-md-4 d-flex justify-content-center align-items-center">
-                            <img src="img/hogar.jpg" class="img-fluid" alt="Imagen de Nosotros">
-                        </div>
-                        <div class="col-md-8">
-                            <h2>Sobre Nosotros</h2>
-                            <p>
-                                Bienvenidos a nuestra página web. Somos una empresa dedicada a brindar soluciones innovadoras en el
-                                mercado.
-                                Nuestro equipo está compuesto por profesionales altamente calificados y apasionados por lo que
-                                hacen.
-                                Nos enorgullece ofrecer productos y servicios de alta calidad que superan las expectativas de
-                                nuestros clientes.
-                            </p>
-                            <p>
-                                Nuestra misión es mejorar continuamente y adaptarnos a las necesidades cambiantes del mercado para
-                                asegurar la satisfacción de nuestros clientes.
-                                Creemos en el valor de la colaboración, la integridad y la excelencia en todo lo que hacemos.
-                            </p>
-                            <p>
-                                Gracias por visitarnos. Esperamos poder trabajar con ustedes y ayudarlos a alcanzar sus objetivos.
-                            </p>
-                        </div>
+                <!-- Added a section element to define a self-contained section -->
+                <div class="main-content">
+                    <div class="container-fluid">
+                        <section class="about-us container my-5">
+                            <div class="row">
+                                <div class="col-md-4 d-flex justify-content-center align-items-center">
+                                    <img src="img/hogar.jpg" class="img-fluid" alt="Imagen de nuestra empresa">
+                                </div>
+                                <div class="col-md-8">
+                                    <h2>Sobre Nosotros</h2>
+                                    <!-- Wrapped the paragraphs in a div with a class to improve readability -->
+                                    <div class="about-us-text">
+                                        <p>
+                                            Bienvenidos a nuestra página web. Somos una empresa dedicada a brindar soluciones innovadoras en el mercado.
+                                            Nuestro equipo está compuesto por profesionales altamente calificados y apasionados por lo que hacen.
+                                            Nos enorgullece ofrecer productos y servicios de alta calidad que superan las expectativas de nuestros clientes.
+                                        </p>
+                                        <p>
+                                            Nuestra misión es mejorar continuamente y adaptarnos a las necesidades cambiantes del mercado para asegurar la satisfacción de nuestros clientes.
+                                            Creemos en el valor de la colaboración, la integridad y la excelencia en todo lo que hacemos.
+                                        </p>
+                                        <p>
+                                            Gracias por visitarnos. Esperamos poder trabajar con ustedes y ayudarlos a alcanzar sus objetivos.
+                                        </p>
+                                    </div>
+                                    <!-- Added a call-to-action button -->
+                                    <a href="#" class="btn btn-primary">Conoce más sobre nosotros</a>
+                                </div>
+                            </div>
+                                <!-- Added a new section for team members -->
+                                <div class="row mt-5">
+                                    <div class="col-md-12">
+                                        <h2>Nuestro Equipo</h2>
+                                        <div class="row">
+                                            <div class="col-md-3 col-sm-6">
+                                                <div class="team-member">
+                                                    <img src="img/p2.jpeg" class="img-fluid" alt="Imagen de Juan Pérez">
+                                                    <h3>Juan Pérez</h3>
+                                                    <p>CEO y Fundador</p>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-3 col-sm-6">
+                                                <div class="team-member">
+                                                    <img src="img/p1.jpg" class="img-fluid" alt="Imagen de María Rodríguez">
+                                                    <h3>María Rodríguez</h3>
+                                                    <p>Diseñadora Gráfica</p>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-3 col-sm-6">
+                                                <div class="team-member">
+                                                    <img src="img/p3.jpg" class="img-fluid" alt="Imagen de Carlos Gómez">
+                                                    <h3>Carlos Gómez</h3>
+                                                    <p>Desarrollador Web</p>
+                                                </div>
+                                            </div>
+                                            <!-- Add more team members here -->
+                                        </div>
+                                    </div>+
+                                </div>
+                                <!-- Added a new section for company history -->
+                                <div class="row mt-12">
+                                    <div class="col-md-12">
+                                    <h2>Nuestra Historia</h2>
+                                    <p>
+                                        Nuestra empresa fue fundada en 2010 con el objetivo de brindar soluciones innovadoras en el mercado.
+                                        Desde entonces, hemos crecido y evolucionado para adaptarnos a las necesidades cambiantes de nuestros clientes.
+                                    </p>
+                                    <p>
+                                        A lo largo de los años, hemos recibido varios premios y reconocimientos por nuestra excelencia en el servicio y nuestra contribución al desarrollo del mercado.
+                                    </p>
+                                    </div>
+                                </div>
+                        </section>
                     </div>
                 </div>
+
+
             <?php include('include/pie_pagina.php'); ?>
                 
 
